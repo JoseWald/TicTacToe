514e4ef96922592f57ede8849bab01589dc94109
JoseWald <rajaonarivonyjosewald@gmail.com>
2024-10-30 15:51:47 +0300

    finishing the intelligent part of the game

--- main.py
+++ main.py
@@ -74,3 +74,63 @@ def check_win(player , check_board = board):
         return True 
     
     return False
+
+#the intelligent part
+def minimax(minimax_board , depth , is_maximising):
+    #Number 1 = the player
+    #Number 2 = the computer
+    if check_win(2 , minimax_board):
+        return float('inf')
+    elif check_win(1 , minimax_board):
+        return float('-inf')
+    elif is_board_full(minimax_board):#no one won
+        return 0
+    
+    if is_maximising :
+        best_score = -1000
+        for row in range(BOARD_ROWS):
+            for col in range(BOARD_COLS):
+                if minimax_board[row][col] == 0 :
+                    minimax_board[row][col] = 2
+                    score = minimax( minimax_board ,  depth + 1 , False)
+                    minimax_board[row][col] = 0
+                    best_score = max(score , best_score)
+        return best_score
+    else:
+        best_score = 1000
+        for row in range(BOARD_ROWS):
+            for col in range(BOARD_COLS):
+                if minimax_board[row][col] == 0 :
+                    minimax_board[row][col] = 1
+                    score = minimax( minimax_board ,  depth + 1 , True)
+                    minimax_board[row][col] = 0
+                    best_score = min(score , best_score)
+        return best_score
+    
+#use the minimax func to actually decide the best move is
+def best_move():
+    best_score = -1000
+    move = ( -1 , -1)
+    for row in range(BOARD_ROWS):
+        for col in range(BOARD_COLS):
+            if board[row][col] == 0 :
+                board[row][col] = 2
+                score = minimax( board , 0 , False)
+                board[row][col] = 0
+                if score > best_score:
+                    best_score = score
+                    move(row , col)
+
+            
+    if move != (-1 , -1):
+        make_square( move[0] , move[1] , 2)
+        return True
+    return False
+
+def restart_game():
+    screen.fill(BLACK)
+    draw_line()
+    for row in range(BOARD_ROWS):
+        for col in range(BOARD_COLS):
+            board[row][col]= 0
+
